bad71233e603438002d6d228e3db75870dabbd3b
Rodrigo Jantsch <rodrigo_jantsch@hotmail.com>
2017-04-06 09:58:53 -0700

    Final Version

--- server.js
+++ server.js
@@ -25,6 +25,6 @@ app.get('/:dateStr', function (req, res) {
   }
 })
 
-app.listen(3000, function () {
+app.listen(process.env.PORT || 5000, function () {
   console.log('Example app listening on port 3000!')
 })
