c734a942384f1c15c627b57ed1eb17febe20aaf5
Rodrigo Jantsch <rodrigo_jantsch@hotmail.com>
2017-04-06 09:52:24 -0700

    Final Version

--- server.js
+++ server.js
@@ -1,8 +1,28 @@
 var express = require('express')
+var moment = require('moment')
 var app = express()
 
-app.get('/', function (req, res) {
-  res.send('Hello World!')
+app.get('/:dateStr', function (req, res) {
+
+
+  var newDate;
+  if(/^\d{8,}$/.test(req.params.dateStr)) {
+    newDate = moment(req.params.dateStr, "X");
+  } else {
+    newDate = moment(req.params.dateStr, "MMMM D, YYYY");
+  }
+
+  if(newDate.isValid()) {
+    res.json({
+      unix: newDate.format("X"),
+      natural: newDate.format("MMMM D, YYYY")
+    });
+  } else {
+    res.json({
+      unix: null,
+      natural: null
+    });
+  }
 })
 
 app.listen(3000, function () {
